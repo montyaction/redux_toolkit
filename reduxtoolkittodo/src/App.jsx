60521b285bb69911617443361a089dcfcd3afcae
montyaction <montyaction@hotmail.com>
2023-11-27 13:43:00 +0530

    redux toolkit project

---
+++ reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import AddTodo from './componet/AddTodo';
+import Todos from './componet/Todos';
+
+function App() {
+
+  return (
+    <>
+        <h1>Learn about redux toolkit</h1>
+        <AddTodo />
+        <Todos />
+    </>
+  )
+}
+
+export default App
